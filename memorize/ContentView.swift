4983ee626c49a8cbb34676951345a095baed2321
tingyu <tingyuz483@gmail.com>
2026-03-16 18:54:44 +0800

    feat: add cardview

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -8,16 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    var body: some View{
+        HStack {
+            CardView(isFaceup: false)
+            CardView()
+            CardView()
+            CardView()
         }
         .padding()
+        .foregroundStyle(.orange)
     }
 }
+struct CardView:View {
+    var isFaceup: Bool=true
+    var body: some View{
+       ZStack {
+            if isFaceup{
+                RoundedRectangle(cornerRadius: 20)
+                    .strokeBorder(lineWidth: 3)
+                Text("🤓")
+           }
+           else {
+               RoundedRectangle(cornerRadius: 20)
+           }
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
