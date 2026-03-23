d9c185e2ef1922053641a85d152142722785ec49
tingyu <tingyuz483@gmail.com>
2026-03-23 18:09:54 +0800

    feat卡片翻面和新增刪

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -8,30 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    //var emojis: Array<String> = ["🐶", "🐱", "🐭", "🐹", "🐰", "🐉", "🐮"]
+    //var emojis:[String] = ["a", "b", "c", "d"]
+    var emojis = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    
+    @State var emojiCount = 6
+    
     var body: some View{
-        HStack {
-            CardView(isFaceup: false)
-            CardView()
-            CardView()
-            CardView()
+        VStack{
+            HStack {
+                ForEach(emojis[0..<emojiCount], id: \.self){emojis in
+                    CardView(content: emojis)
+                }
+            }
+            HStack{
+                Button(action: {
+                    emojiCount -= 1
+                }, label: {
+                    Text("Remove Card")
+                })
+                
+                Spacer()
+                
+                Button(action: {
+                    emojiCount += 1
+                }, label: {
+                    Text("Add Card")
+                })
+            }
         }
         .padding()
         .foregroundStyle(.orange)
     }
 }
 struct CardView:View {
-    var isFaceup: Bool=true
+    @State var isFaceup: Bool=true
+    var content: String
     var body: some View{
        ZStack {
-            if isFaceup{
-                RoundedRectangle(cornerRadius: 20)
-                    .strokeBorder(lineWidth: 3)
-                Text("🤓")
+           //var shape:RoundedRectangle = RoundedRectangle(cornerRadius: 20)
+           let shape = RoundedRectangle(cornerRadius: 20)
+           //var shape = Circle();
+           if isFaceup{
+                shape.fill(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
            }
            else {
-               RoundedRectangle(cornerRadius: 20)
+               shape
            }
         }
+       .onTapGesture {
+           isFaceup = !isFaceup
+       }
+        
+//     .onTapGesture(perform:{
+//         isFaceup = !isFaceup
+//       })
     }
 }
 
